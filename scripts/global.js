b9f3b8a79b736cf7e21eec8e79f035b6d9a0c98a
Luis Felipe Silva Elizeu <l_felipelizeu@hotmail.com>
2021-08-11 18:34:43 -0300

    Todos os quizzes carregados normalmente, ainda falata a lógica de separar os quizzes feitos pelo usuário de todos os quizzes

--- scripts/global.js
+++ scripts/global.js
@@ -2,15 +2,36 @@ const API_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/qu
 
 let quizzes;
 
-function renderQuizzes() {
+function renderCreatedQuizzes() {
     const id_created = JSON.parse(localStorage.getItem("id"));
 
     const created = document.querySelector(".list-screen .created-quizzes");
 
-    if ( id_created === undefined ) {
+    if ( id_created !== null ) {
         const no_created_quizz = created.querySelector(".no-created-quizz");
         no_created_quizz.classList.add("hidden");
+
+        element.innerHTML = `<div class="new-quizz"><h1>Seus Quizzes</h1><ion-icon name="add-circle"></ion-icon></div><ul>`;
+
+        //Procurar os ids do localStorage que estão no array quizzes
+        //element.innerHTML += </ul>; //ao fim do for
     }
+}
+
+function renderQuizzes() {
+    renderCreatedQuizzes();
+
+    const all_quizzes_list = document.querySelector(".list-screen .all-quizzes ul");
+
+    for ( let i = 0; i < quizzes.length; i++ ) {
+        all_quizzes_list.innerHTML += `<li class="quizz">
+        <img src="${quizzes[i].image}" />
+        <div class="gradient"></div>
+        <span class="title">${quizzes[i].title}</span>
+        </li>`
+    }
+
+    //Ainda falta a lógica de não colocar aqui os quizzes criados pelo usuário
 }
 
 function loadQuizzes(object) {
