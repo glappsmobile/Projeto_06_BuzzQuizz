d2cbef86f0f2332650a5d6489ea5520c615c5a16
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-08-11 12:58:35 -0300

    Removi funções que vieram por engano de outro projeto

--- scripts/main.js
+++ scripts/main.js
@@ -3,28 +3,7 @@ function onLoad(){
 }
 
 function initialConfig(){
-    const inputMessage = document.querySelector(".container-send-message input.send");
-    const inputName = document.querySelector(".window-login input.join");
-    inputName.focus();
-    changeButtonAndInputState(DISABLED, ENABLED);
 
-    inputName.addEventListener("keyup", function(event) {
-        changeButtonAndInputState(AUTO, ENABLED);
-
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            if (buttonsStates[0].state) { joinRoom(); }
-        }
-    });
-
-    inputMessage.addEventListener("keyup", function(event) {
-        changeButtonAndInputState(AUTO, ENABLED);
-
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            if (buttonsStates[1].state) { sendMessage(); }
-        }
-    });
 }
 
 
