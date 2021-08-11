cb40e6859fd5d2c893dd890c42491cce92b78f49
Luis Felipe Silva Elizeu <l_felipelizeu@hotmail.com>
2021-08-11 17:02:31 -0300

    Adicionei a classe hidden para esconder as coisas e comecei a fazer a função de carregar os quizzes

--- scripts/global.js
+++ scripts/global.js
@@ -1,1 +1,28 @@
 const API_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes";
+
+let quizzes;
+
+function renderQuizzes() {
+    const id_created = JSON.parse(localStorage.getItem("id"));
+
+    const created = document.querySelector(".list-screen .created-quizzes");
+
+    if ( id_created === undefined ) {
+        const no_created_quizz = created.querySelector(".no-created-quizz");
+        no_created_quizz.classList.add("hidden");
+    }
+}
+
+function loadQuizzes(object) {
+    quizzes = object.data;
+
+    renderQuizzes();
+}
+
+function getQuizzes() {
+    const promise = axios.get(API_URL);
+
+    promise.then(loadQuizzes);
+}
+
+getQuizzes();
