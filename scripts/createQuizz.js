0f12593a1de58371d5f7ceb66e25be08ada1f067
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-08-12 15:55:51 -0300

    fixed class conflicts

--- scripts/createQuizz.js
+++ scripts/createQuizz.js
@@ -1,10 +1,25 @@
 const quizzInCreation = {}
 
+function inputNumberCheck(input, min, max){
+
+    if (input.value.length === 0 ) { return; }
+    if (input.value < min) {
+        input.value = min;
+    } else if (input.value > max){
+        input.value = max;
+    }
+}
+
+function inputLengthCheck(input, minLength){
+    //por enquanto ta com alerta, mas será trocado por um label de erro em cima do input
+  //  if (input.value.length < minLength) { alert("O tamanho deve ser no mínimo "+minLength)}
+}
+
 function goToCreateQuizz(){
     openScreen(SCREENS.CREATE_QUIZZ);
     openSubscreen(SUBSCREENS.CREATE_BASIC);
 }
-
+goToCreateQuizz()
 function goToQuizzList(){
     openScreen(SCREENS.QUIZZ_LIST);
 }
