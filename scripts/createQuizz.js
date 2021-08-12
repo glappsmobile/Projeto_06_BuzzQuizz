6e55599b35edd007211610739f5e51ce86d2fa55
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-08-12 13:54:48 -0300

    Função goToNextPage() criada e adicionada nos onclicks

--- scripts/createQuizz.js
+++ scripts/createQuizz.js
@@ -56,3 +56,29 @@ function goToCreateQuizz(){
     openScreen(SCREENS.CREATE_QUIZZ);
     openSubscreen(SUBSCREENS.CREATE_BASIC);
 }
+
+function goToQuizzList(){
+    openScreen(SCREENS.QUIZZ_LIST);
+}
+
+
+function goToNextPage(){
+    if (current.screen === SCREENS.CREATE_QUIZZ){
+
+        if (current.subscreen === SUBSCREENS.CREATE_BASIC) {
+            openSubscreen(SUBSCREENS.CREATE_QUESTIONS);
+            return;
+        } 
+
+        if (current.subscreen === SUBSCREENS.CREATE_QUESTIONS) {
+            openSubscreen(SUBSCREENS.CREATE_LEVELS);
+            return;
+        }
+
+        if (current.subscreen === SUBSCREENS.CREATE_LEVELS) {
+            openSubscreen(SUBSCREENS.CREATE_SUCCESS);
+            return;
+        }
+
+    }
+}
