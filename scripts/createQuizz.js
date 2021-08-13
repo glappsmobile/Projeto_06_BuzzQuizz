0e2078aef2d700e927f3c43692d3936db3301950
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-08-12 21:57:12 -0300

    Iniciada a exibição de erros em inputs

--- scripts/createQuizz.js
+++ scripts/createQuizz.js
@@ -1,18 +1,55 @@
 const quizzInCreation = {}
 
-function inputNumberCheck(input, min, max){
 
-    if (input.value.length === 0 ) { return; }
+function setInputError(input, errorMessage){
+    let parent = input.parentElement;
+    let span = parent.querySelector(`span.error`);
+
+    if (errorMessage.length > 0){
+        span.innerHTML = errorMessage;
+        input.classList.add("invalid");
+    } else {
+        span.innerHTML = "";
+        input.classList.remove("invalid");
+    }
+}
+
+function inputNumberCheck(input, min, max, showError){
+    if (input.value.length === 0 ) { 
+        if (showError) { setInputError(input, `A quantidade deve ser entre ${min} e ${max}`) };
+        return false; 
+    }
+
     if (input.value < min) {
         input.value = min;
     } else if (input.value > max){
         input.value = max;
     }
+
+    setInputError(input, "");
+    return true;
 }
 
-function inputLengthCheck(input, minLength){
-    //por enquanto ta com alerta, mas será trocado por um label de erro em cima do input
-  //  if (input.value.length < minLength) { alert("O tamanho deve ser no mínimo "+minLength)}
+function inputMinLengthCheck(input, minLength, showError){
+    if (input.value.length < minLength) { 
+        if (showError) { setInputError(input, `O texto deve conter no mínimo ${minLength} letras`); }
+        return false;
+    } else {
+        setInputError(input, false);
+        return true;
+    }
+}
+
+function inputUrlCheck(input, showError){
+    const matchpattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
+
+    if (!matchpattern.test(input.value)){
+        if (showError) { setInputError(input, "O valor informado não é uma URL válida"); }
+        return false;
+    } else {
+        setInputError(input, false);
+        return true;
+    }
 }
 
 function goToCreateQuizz(){
