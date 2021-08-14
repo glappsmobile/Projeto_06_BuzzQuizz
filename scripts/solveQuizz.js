e80ed416474204422c26cc1c76e8ce4f80959cc4
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-08-13 23:42:15 -0300

    Lógica do resultado do quizz implementada

--- scripts/solveQuizz.js
+++ scripts/solveQuizz.js
@@ -1,23 +1,22 @@
 let rightAnswers = 0;
-
 const getTotalAnswered = () => document.querySelectorAll(".answered").length;
 const isQuizzFinished = () => getTotalAnswered() === thisQuizz.questions.length;
 const getRate = () => Number(((rightAnswers/thisQuizz.questions.length)*100).toFixed());
+const getContrastColor = (hexColor) => (isHexColorBright(hexColor)) ? "#000000" : "#FFFFFF";
 
 const renderQuizzResult = () => {
     const container = document.querySelector(`.${SCREENS.QUIZZ_QUESTIONS} ul.questions`);
 
+    const level = thisQuizz.levels.sort((a, b) => b.minValue - a.minValue).filter((item) => item.minValue <= getRate())[0];
 
     container.innerHTML += ` 
-    <div class="question result">
-        <div class="title">${getRate()}% de acerto</div>
+    <li class="question result">
+        <div class="title white">${getRate()}% de acerto: ${level.title}</div>
         <div class="result-box"> 
-        <img src="${thisQuizz.levels[0].image}" />
-        <p>${thisQuizz.levels[0].text}</p>
+        <img src="${level.image}" />
+        <p>${level.text}</p>
         </div>
-    </div>`
-
-    setTimeout(() => {container.scrollIntoView({block: "end", behavior: "smooth"})}, 2000);
+    </li>`
 }
 
 function nextQuestion() {
@@ -26,8 +25,9 @@ function nextQuestion() {
     for ( let i = 0; i < questions.length; i++ ) {
         const options = questions[i].querySelector(".options");
 
-        if (!options.classList.contains("answered")) {
-            questions[i].scrollIntoView({block: "end", behavior: "smooth"});
+        questions[i].scrollIntoView({block: "end", behavior: "smooth"});
+
+        if (options !== null && !options.classList.contains("answered")) {
             break;
         }
     }
@@ -50,11 +50,10 @@ function correctQuestion(chosenOption, indexQuestion){
 
    chosenOption.classList.remove("blur");
 
-   if (isQuizzFinished()) {
-        renderQuizzResult();
-    } else {
-        setTimeout(nextQuestion, 2000);
-    }
+   if (isQuizzFinished()) { renderQuizzResult(); }
+
+    setTimeout(nextQuestion, 2000);
+
 }
 
 function renderQuestions(){
@@ -63,9 +62,6 @@ function renderQuestions(){
     const banner = screen.querySelector(".banner");
     const bannerImg = banner.querySelector("img");
     const bannerTxt = banner.querySelector("h1");
-
-    const black = "#000000";
-    const white = "#FFFFFF";
     let htmlList = "";
 
     bannerImg.src = thisQuizz.image;
@@ -73,7 +69,8 @@ function renderQuestions(){
 
     thisQuizz.questions.forEach((question, indexQuestion) => {
 
-        const textColor = (isHexColorBright(question.color)) ? black : white;
+        const textColor = getContrastColor(question.color);
+
         htmlList += `
         <li class="question">
             <div class="title" style="color: ${textColor}; background-color: ${question.color}">${question.title}</div>
